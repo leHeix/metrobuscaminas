073e1e4c6b5b875c33bd2f001c9d6fe8c68cd410
leHeix <gordosatiro@protonmail.com>
2025-02-27 20:54:56 -0400

    Asignar minas a casillas

--- src/metrobuscaminas/Game.java
+++ src/metrobuscaminas/Game.java
@@ -20,17 +20,47 @@ public class Game {
     
     public Game(MainMenu menu, int row_count, int column_count, int mine_count)
     {
-        this.window = new GameWindow(menu);
         this.row_count = row_count;
         this.column_count = column_count;
         this.mine_count = mine_count;
+        this.mines = new List<Boolean>();
+       
+        this.assign_mines();
         
-        int box_count = this.row_count * this.column_count;
-        while(box_count < 0)
+        this.window = new GameWindow(menu);
+    }
+    
+    private void assign_mines()
+    {
+        int fixed_box_count = this.column_count * this.row_count;
+        int box_count = fixed_box_count;
+        
+        while(box_count > 0)
         {
-            this.mines.insert_back(false);
+            this.mines.insert_back(fixed_box_count == this.mine_count ? true : false);
             box_count--;
         }
+        
+        if(fixed_box_count == this.mine_count)
+            return;
+        
+        Random rng = new Random();
+        int assigned_mines = 0;
+        
+        do
+        {
+            int box;
+            
+            do
+            {
+                box = rng.nextInt(fixed_box_count);
+            }
+            while(this.mines.get(box).get() == true);
+            
+            this.mines.set(box, true);
+            assigned_mines++;
+        }
+        while(assigned_mines < this.mine_count);
     }
     
     public void show_game_window()
